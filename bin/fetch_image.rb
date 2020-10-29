ee6cdfe59ecad89444eb2affd8901b88c1f396cd
Fayme Shahriar <fayme.shahriar@gmail.com>
2020-10-29 22:33:40 +0100

    Formatted code

--- bin/fetch_image.rb
+++ bin/fetch_image.rb
@@ -2,13 +2,12 @@
 require "down"
 require "fileutils"
 
-open("./test_file.txt") do |file|
-    file.each_line do |line|
-        encoded_link = URI::encode(line.strip)
-        puts encoded_link #image link
-        tempfile = Down.download("#{encoded_link}", max_size: 5 * 1024 * 1024) # Restricting file size
-        Dir.mkdir("./images") unless File.exists?("./images")
-        FileUtils.mv(tempfile.path, "./images/#{tempfile.original_filename}")
-    end
-    
-  end
+open("./test_file.txt").each_line do |line|
+
+    encoded_link = URI::encode(line.strip)
+    puts encoded_link #image link
+    tempfile = Down.download("#{encoded_link}", max_size: 5 * 1024 * 1024) # Restricting file size
+    Dir.mkdir("./images") unless File.exists?("./images")
+    FileUtils.mv(tempfile.path, "./images/#{tempfile.original_filename}")
+
+end

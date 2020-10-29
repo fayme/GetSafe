7d6a158db4aa7db81763fdf31890b50a2acbd5dc
Fayme Shahriar <fayme.shahriar@gmail.com>
2020-10-29 14:49:02 +0100

    Command-Line Script to download image from link

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GetSafe
+
+Before running the command, make it executable:
+
+chmod +x bin/fetch_image.rb
+
+Then run:
+
+bin/fetch_image.rb
+
+Used Gem:
+
+Down: https://github.com/janko/down
---
+++ bin/fetch_image.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require "down"
+require "fileutils"
+
+open("./test_file.txt") do |file|
+    file.each_line do |line|
+        encoded_link = URI::encode(line.strip)
+        puts encoded_link #image link
+        tempfile = Down.download("#{encoded_link}", max_size: 5 * 1024 * 1024) # Restricting file size
+        FileUtils.mv(tempfile.path, "./images/#{tempfile.original_filename}")
+    end
+    
+  end
